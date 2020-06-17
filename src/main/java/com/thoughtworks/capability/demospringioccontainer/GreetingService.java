0fe9fb479d1fb1e46c982dc5ff9f5426248f5259
JingeLi <jinge.li@thoughtworks.com>
2020-06-17 18:52:12 +0800

    set Scope prototype

--- src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingService.java
+++ src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingService.java
@@ -1,9 +1,15 @@
 package com.thoughtworks.capability.demospringioccontainer;
 
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Service;
 
+@Scope(value = "prototype", proxyMode = ScopedProxyMode.TARGET_CLASS)
 @Service
 public class GreetingService {
+    public GreetingService() {
+        System.out.println("***********GreetingService()***********");
+    }
 
     String sayHi() {
         return "hello world";
