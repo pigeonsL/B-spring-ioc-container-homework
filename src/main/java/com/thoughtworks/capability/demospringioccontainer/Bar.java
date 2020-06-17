12327947ea248b9ba75380f1608c81b5c5a76c9a
JingeLi <jinge.li@thoughtworks.com>
2020-06-17 18:59:11 +0800

    Setter injection

--- src/main/java/com/thoughtworks/capability/demospringioccontainer/Bar.java
+++ src/main/java/com/thoughtworks/capability/demospringioccontainer/Bar.java
@@ -1,7 +1,6 @@
 package com.thoughtworks.capability.demospringioccontainer;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -10,7 +9,7 @@ public class Bar {
     private Foo foo;
 
     @Autowired
-    public Bar(@Lazy Foo foo) {
+    public void setFoo(Foo foo) {
         this.foo = foo;
     }
 }
--- src/main/java/com/thoughtworks/capability/demospringioccontainer/Foo.java
+++ src/main/java/com/thoughtworks/capability/demospringioccontainer/Foo.java
@@ -1,13 +1,14 @@
 package com.thoughtworks.capability.demospringioccontainer;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Foo {
 
     private Bar bar;
-
-    public Foo(Bar bar) {
+    @Autowired
+    public void setBar(Bar bar) {
         this.bar = bar;
     }
 }
